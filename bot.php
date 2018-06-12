46a1fdf3b3797cdf2f0e0267d49aed163c157778
DESKTOP-3AGPC0J\SuPeRbEeR <anucha.ka@inet.co.th>
2018-06-12 23:34:28 +0700

    ต่อ curl

--- bot.php
+++ bot.php
@@ -22,9 +22,20 @@ if (!is_null($events['events'])) {
 			if (is_numeric($text)){
 				$size = strlen($text);
 				if ($size == 13) {
+					$url = 'https://bcca.thaijobjob.com/201806/line/search.php'; // กำหนด URl
+					$request = 'cus_id='.$text.''; // กำหนด HTTP Request โดยระบุ username=guest และ password=เguest (รูปแบบเหมือนการส่งค่า $_GET แต่ข้างหน้าข้อความไม่มีเครื่องหมาย ?)
+					$ch = curl_init(); // เริ่มต้นใช้งาน cURL
+					curl_setopt($ch, CURLOPT_URL, $url); // กำหนดค่า URL
+					curl_setopt($ch, CURLOPT_POST, 1); // กำหนดรูปแบบการส่งข้อมูลเป็นแบบ $_POST
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $request); // กำหนดค่า HTTP Request
+					curl_setopt($ch, CURLOPT_HEADER, 0); // กำให้ cURL ไม่มีการตั้งค่า Header
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // กำหนดให้ cURL คืนค่าผลลัพท์
+					$response = curl_exec($ch); // ประมวลผล cURL
+					curl_close($ch); // ปิดการใช้งาน cURL
+					// echo $response; // แสดงผลการทำงาน
 					$messages = [
 						'type' => 'text',
-						'text' => "สวัสดีคุณ user: $user_token"
+						'text' => $response['fullname'];
 					];
 				} else {
 					$messages = [
