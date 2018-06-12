af6eaf6357bfbd7bf4e6b0436b56d5baaa37d9bc
Anucha Kaewkun <anucha.ka@inet.co.th>
2018-06-12 13:02:20 +0700

    Test Line Bot

--- bot.php
+++ bot.php
@@ -17,10 +17,18 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+			// Build message to reply back
+			if ($text == "hello") {
+				$messages = [
+					'type' => 'text',
+					'text' => "สวัสดี SuPeRbEeR"
+				];
+			} else {
+				$messages = [
+					'type' => 'text',
+					'text' => $text
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
