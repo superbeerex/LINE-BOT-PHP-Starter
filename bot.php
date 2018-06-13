87910c201602bc21296b922285446adf50cfa7d8
Anucha Kaewkun <anucha.ka@inet.co.th>
2018-06-13 14:35:13 +0700

    revert

--- bot.php
+++ bot.php
@@ -22,37 +22,20 @@ if (!is_null($events['events'])) {
 			if (is_numeric($text)){
 				$size = strlen($text);
 				if ($size == 13) {
-					// $url = 'https://bcca.thaijobjob.com/201806/line/select.php'; // กำหนด URl
-					// $request = 'cus_id='.$text.''; // กำหนด HTTP Request โดยระบุ username=guest และ password=เguest (รูปแบบเหมือนการส่งค่า $_GET แต่ข้างหน้าข้อความไม่มีเครื่องหมาย ?)
-					// $ch = curl_init(); // เริ่มต้นใช้งาน cURL
-					// curl_setopt($ch, CURLOPT_URL, $url); // กำหนดค่า URL
-					// curl_setopt($ch, CURLOPT_POST, 1); // กำหนดรูปแบบการส่งข้อมูลเป็นแบบ $_POST
-					// curl_setopt($ch, CURLOPT_POSTFIELDS, $request); // กำหนดค่า HTTP Request
-					// curl_setopt($ch, CURLOPT_HEADER, 0); // กำให้ cURL ไม่มีการตั้งค่า Header
-					// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // กำหนดให้ cURL คืนค่าผลลัพท์
-					// $response = curl_exec($ch); // ประมวลผล cURL
-					// curl_close($ch); // ปิดการใช้งาน cURL
+					$url = 'https://bcca.thaijobjob.com/201806/line/select.php'; // กำหนด URl
+					$request = 'cus_id='.$text.''; // กำหนด HTTP Request โดยระบุ username=guest และ password=เguest (รูปแบบเหมือนการส่งค่า $_GET แต่ข้างหน้าข้อความไม่มีเครื่องหมาย ?)
+					$ch = curl_init(); // เริ่มต้นใช้งาน cURL
+					curl_setopt($ch, CURLOPT_URL, $url); // กำหนดค่า URL
+					curl_setopt($ch, CURLOPT_POST, 1); // กำหนดรูปแบบการส่งข้อมูลเป็นแบบ $_POST
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $request); // กำหนดค่า HTTP Request
+					curl_setopt($ch, CURLOPT_HEADER, 0); // กำให้ cURL ไม่มีการตั้งค่า Header
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // กำหนดให้ cURL คืนค่าผลลัพท์
+					$response = curl_exec($ch); // ประมวลผล cURL
+					curl_close($ch); // ปิดการใช้งาน cURL
 					// echo $response; // แสดงผลการทำงาน
 					$messages = [
-						// 'type' => 'text',
-						// 'text' => "เลขบัตรประจำตัวประชาชน: ".$text."\r\n".$response
-						"type": "template",
-					  "altText": "this is a confirm template",
-					  "template": {
-					      "type": "confirm",
-					      "text": "Are you sure?",
-					      "actions": [
-					          {
-					            "type": "message",
-					            "label": "Yes",
-					            "text": "yes"
-					          },
-					          {
-					            "type": "message",
-					            "label": "No",
-					            "text": "no"
-					          }
-					      ]
+						'type' => 'text',
+						'text' => "เลขบัตรประจำตัวประชาชน: ".$text."\r\n".$response
 					];
 				} else {
 					$messages = [
